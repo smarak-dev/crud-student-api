1a43446f95c1db60fcf8b08055e8119865e144ab
smarak-dev <smarak.pani.82@gmail.com>
2023-04-04 23:35:07 +0530

    initial version
    initial version

---
+++ src/test/java/com/app/crud/CrudStudentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudStudentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
